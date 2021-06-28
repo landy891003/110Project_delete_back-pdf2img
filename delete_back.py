c34b959bba06a01fc437f6fc36038d038bdfc741
landy891003 <landy891003@gmail.com>
2021-06-28 21:35:27 +0800

    去背

--- delete_back.py
+++ delete_back.py
@@ -15,12 +15,12 @@ n=0
 i=0
 while n<len(num):
     try:
-        image = 'C:\\Users\\USER\\Desktop\\heart\\heart_diagram\\heart_pre\\'+num[n]+'_pre.jpg'
+        image = 'C:\\Users\\USER\\Desktop\\heart\\heart_diagram\\heart_pre\\'+num[n]+'_pre.jpg'     #修改此處
         img = cv2.imread(image)
         hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
         mask = cv2.inRange(hsv, redLower, redUpper)
         mask1 = cv2.bitwise_not(mask,mask)
-        cv2.imwrite('C:\\Users\\USER\\Desktop\\heart\\heart_diagram\\heart_pre\\'+num[n]+'_pre.jpg', mask1)
+        cv2.imwrite('C:\\Users\\USER\\Desktop\\heart\\heart_diagram\\heart_pre\\'+num[n]+'_pre.jpg', mask1) #修改此處
         cv2.waitKey(0)
         print(num[n],"finished delete back",i)
         i=i+1
