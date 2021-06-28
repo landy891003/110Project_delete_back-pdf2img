cfb634f471a3be5e7dd8b3dbb689d27c9097234e
landy891003 <landy891003@gmail.com>
2021-06-28 21:34:48 +0800

    轉檔

--- pdf_to_jpg.py
+++ pdf_to_jpg.py
@@ -15,9 +15,9 @@ n=0
 i=0
 while(n<len(num)):
     try:
-        heart_path="C:\\Users\\USER\\Desktop\\heart\\heart_diagram\\heart_pic_pre\\EKG_"+num[n]+"_pre.pdf"
-        pages = convert_from_path(heart_path,fmt='jpeg',poppler_path = r"C:\\Users\\USER\\Desktop\\Python 3.9\\poppler-21.03.0\\Library\\bin")
-        pages[0].save("C:\\Users\\USER\\Desktop\\heart\\heart_diagram\\heart_pre\\"+num[n]+'_pre.jpg','JPEG')
+        heart_path="C:\\Users\\USER\\Desktop\\heart\\heart_diagram\\heart_pic_pre\\EKG_"+num[n]+"_pre.pdf"#修改此處
+        pages = convert_from_path(heart_path,fmt='jpeg',poppler_path = r"C:\\Users\\USER\\Desktop\\Python 3.9\\poppler-21.03.0\\Library\\bin")  #此處放poppler的路徑
+        pages[0].save("C:\\Users\\USER\\Desktop\\heart\\heart_diagram\\heart_pre\\"+num[n]+'_pre.jpg','JPEG')#修改此處
         print(num[n],"次數:",i)
         i=i+1
     except Exception as e:
